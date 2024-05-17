abe5d42113affd35351743b21e767c0de24655ee
priyamsharma2704 <priyamsharma2704@gmail.com>
2024-05-17 09:36:44 -0400

    updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "es2022"
+  },
+  esbuild: {
+    target: "es2022"
+  },
+  optimizeDeps:{
+    esbuildOptions: {
+      target: "es2022",
+    }
+  }
 })
